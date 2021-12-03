2637511ec8d5eba8d88778cf296d2fba696f86d3
mong-head <camara1melon315@gmail.com>
2021-12-03 14:16:52 +0900

    [learn] todoApp #1

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import CounterContainer from './containers/CounterContainer';
+import TodoApp from './containers/TodoApp';
 
 function App() {
-  return <CounterContainer />;
+  const counterApp = <CounterContainer />;
+  const todoApp = <TodoApp />;
+
+  return todoApp;
 }
 
 export default App;
